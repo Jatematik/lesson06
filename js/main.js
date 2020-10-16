4894a44f8e42c8dbca5bdbe79542943330db9368
Jatematik <freerun_dimon@mail.ru>
2020-10-16 20:45:47 +0600

    feat: created the game

--- js/main.js
+++ js/main.js
@@ -4,22 +4,30 @@ let isNumber = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let myNum = prompt('Угадай число от 1 до 100'); // Исправить на 100
-let a = Number(myNum);
-let b = Math.floor(Math.random() * 100);
-console.log(b);
-// console.log(typeof a);
 let getNumber = function (){
-    for (let i = 0; i < 100; i++){
-        if (a > b) {
-            myNum = prompt('Загаданное число меньше, введите другой вариант');
-            a = Number(myNum);
-            console.log(typeof a);
-        } else if (a < b) {
-            myNum = prompt('Загаданное число больше, введите другой вариант');
-            a = Number(myNum);
-            console.log(typeof a);
-        }
-    }
+    let myNum = prompt('Угадай число от 1 до 100');
+    console.log(myNum);
+    let b = Math.floor(Math.random() * 100);
+    console.log(b);
+    return function(){
+        for (let i = 0; i < Infinity; i++){
+            if (myNum === null) {
+                alert('Игра окончена');
+                break;
+            } else if (Number(myNum) === b) {
+                alert('Поздравляю, Вы угадали число!!!');
+                break;
+            } else if (myNum > b) {
+                myNum = prompt('Загаданное число меньше, введите другой вариант');
+            } else if (myNum < b) {
+                myNum = prompt('Загаданное число больше, введите другой вариант');
+            } else if (!isNumber(myNum)) {
+                myNum = prompt('Введите число!');
+            } 
+        }   
+    };
 };
-getNumber();
+
+let getRandomNum = getNumber();
+getRandomNum();
+console.dir(getRandomNum);
